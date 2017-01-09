26a314d269398ab42d7cd7e5a7ad41eac4e4423c
samir <sjha4@ncsu.edu>
2017-01-09 14:34:39 -0500

    Adding Music App

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
-  get '/music', 'application#music'
+  get '/music' => 'application#music'
   get 'message/new' => 'comments#new'
   post 'application/new_comment' => 'application#new_comment'
   resources :comments
