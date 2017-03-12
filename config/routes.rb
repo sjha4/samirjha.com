6e2c4439c8b04922ed6b2cf99128b04e7937657f
samir <sjha4@ncsu.edu>
2017-03-12 07:52:45 -0400

    Adding background video

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
   get '/music' => 'application#music'
+  get '/home' => 'application#home'
   get 'message/new' => 'comments#new'
   post 'application/new_comment' => 'application#new_comment'
   resources :comments
