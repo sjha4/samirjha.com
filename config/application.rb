ef507b67a98c2c271116238d6ed03eb37fb1b6a1
Samir Jha <sajha@localhost.localdomain>
2018-04-30 21:56:14 -0400

    Trying config

--- config/application.rb
+++ config/application.rb
@@ -5,9 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOWALL'
-}
+
 module Untitled6
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
