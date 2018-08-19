52fe735e4ed6cd89fa8285c499b39804c1e49453
Samir Jha <samirjha1525@gmail.com>
2018-08-18 22:30:34 -0400

    Changing home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#video'
+  root 'application#home'
   get '/music' => 'application#music'
   get '/home' => 'application#home'
   get 'message/new' => 'comments#new'
