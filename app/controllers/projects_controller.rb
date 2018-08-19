80433efd6cb78c94be58978372d371a277912235
Samir Jha <samirjha1525@gmail.com>
2018-08-19 01:03:09 -0400

    Add project

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,16 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  def index
+    @projects = Project.all
+  end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def project_params
+    params.require(:comment).permit(:Name, :Comment, :ContactInfo)
+  end
+end
---
+++ db/migrate/20180819044938_create_projects.rb
@@ -0,0 +1,10 @@
+class CreateProjects < ActiveRecord::Migration[5.0]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.string :description
+      t.string :image
+      t.string :link
+    end
+  end
+end
